f7e2550b7a4a074a1bfbf1c5033d02abd63a1ba4
Elena <agapitovae87@gmail.com>
2022-09-30 11:11:12 +0300

    Task 1

--- Seminar1/Seminar/Program.cs
+++ Seminar1/Seminar/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input a number1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a number2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quard2 = n2 * n2;
+
+if (n1 == quard2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
