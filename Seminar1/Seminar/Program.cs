6dc8b8fd1ead199fb7bae02c5eb2cfc3427ab91a
Elena <agapitovae87@gmail.com>
2022-09-30 11:58:01 +0300

    Task 2

--- Seminar1/Seminar/Program.cs
+++ Seminar1/Seminar/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Input a number1: ");
+﻿/*
+// Task 1.
+Console.Write("Input a number1: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Input a number2: ");
@@ -14,4 +16,22 @@ else
 {
     Console.WriteLine("No");
 }
+*/
 
+/*
+// Task 2. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// последнюю цифру этого числа.
+
+Console.Write("Input a three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>=100 && n<1000)
+{
+    int lastDigit = n % 10;
+    Console.WriteLine($"Last digit of {n} is {lastDigit}");
+}
+else
+{
+   Console.WriteLine("Uncorrent input");
+}
+*/
