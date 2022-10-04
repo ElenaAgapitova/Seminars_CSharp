c61d6e49435b2edc9278b31a71a53ca6722c4207
Elena <agapitovae87@gmail.com>
2022-10-04 14:56:40 +0300

    Homework 2 Task 2 way 2

--- HomeWork/HomeWork2/Program.cs
+++ HomeWork/HomeWork2/Program.cs
@@ -21,6 +21,8 @@ Console.ReadLine();
 
 // Задача №2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 // что третьей цифры нет.
+
+//Решение 1:
 /*
 Console.Clear();
 
@@ -46,8 +48,39 @@ if (num>=1 & num<=99)
 else
 {
     int answer = ThirdDigit(num);
-    Console.WriteLine(num);
-    Console.WriteLine(answer);
+    Console.WriteLine($"Полученное число = {num}, третья цифра в данном числе = {answer}");
+}
+Console.ReadLine();
+*/
+
+// Решение 2:
+/*
+Console.Clear();
+
+int ThirdDigit(int num)
+{
+    int res = 0;
+
+    if (num >= 100 & num <= 999)
+        res = num % 10;
+    else 
+        if (num >= 1000 & num <= 9999)
+            res = (num / 10) % 10;
+    else 
+        if (num >= 10000 & num <= 99999)
+            res = (num / 100) % 10;
+    return res;
+}
+
+Console.Write("Введите число от 1 до 99 999: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num>=1 & num<=99)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+    int answer = ThirdDigit(num);
+    Console.WriteLine($"Полученное число = {num}, третья цифра в данном числе = {answer}");
 }
 Console.ReadLine();
 */
