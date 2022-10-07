0d49b7a40d3edce9ee0e48fc20a3f56c0074a31c
Elena <agapitovae87@gmail.com>
2022-10-07 14:10:05 +0300

    Task 1 way 2

--- HomeWork/Homework3/Program.cs
+++ HomeWork/Homework3/Program.cs
@@ -31,5 +31,24 @@ Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine(result);
 Console.ReadLine();
 */
+// Решение 2:
 
+Console.Clear();
+
+void Palindrome(int num)
+{
+    int symbol_1 = num / 10000;
+    int symbol_5 = num % 10;
+    int symbol_2 = num / 1000 % 10;
+    int symbol_4 = num / 10 % 10;
+    if(symbol_1==symbol_5 && symbol_2==symbol_4)
+        Console.WriteLine($"Число {num} - палиндром.");
+    else
+        Console.WriteLine($"Число {num} НЕ является палиндромом.");
+}
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrome(num);
+Console.ReadLine();
 
