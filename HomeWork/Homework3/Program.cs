bf913d406b29ecad4358790cc2488659e657ae0a
Elena <agapitovae87@gmail.com>
2022-10-07 14:46:43 +0300

    Task 2

--- HomeWork/Homework3/Program.cs
+++ HomeWork/Homework3/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача №1. 
 /*
+// Решение 1:
 Console.Clear();
 
 string Palindrome(int num)
@@ -31,8 +32,8 @@ Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine(result);
 Console.ReadLine();
 */
+/*
 // Решение 2:
-
 Console.Clear();
 
 void Palindrome(int num)
@@ -51,11 +52,39 @@ Console.Write("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Palindrome(num);
 Console.ReadLine();
+*/
+/*
+// Задача №2. Напишите программу, которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 3D пространстве.
 
+Console.Clear();
 
+double FindDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+    return distance;
+}
 
+Console.Write("Введите кординату x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
 
+Console.Write("Введите кординату x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
 
+double distance = FindDistance(x1,y1,z1,x2,y2,z2);
+double Distance = Math.Round(distance,2);
+Console.WriteLine($"Растояние между точкой А({x1},{y1},{z1}) и точкой B({x2},{y2},{z2}) = {Distance}");
+
+Console.ReadLine();
+*/
 // Задача №3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов 
 // чисел от 1 до N.
 
