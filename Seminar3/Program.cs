9f2fdf802f6c82f429ee3314c9e5c6379402aa4a
Elena <agapitovae87@gmail.com>
2022-10-07 10:47:30 +0300

    Task1

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1. Напишите программу, которая принимает на вход координаты точки (X и Y), причём 
+// X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int FindQuart(double x, double y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.Write("Input x-coordinate: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y-coordinate: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"The point A({x},{y}) is on the {FindQuart(x,y)} quart");
