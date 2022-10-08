4a035d8d4803a36b7e8838b9f6796a279ae7a64c
Elena <agapitovae87@gmail.com>
2022-10-08 17:31:21 +0300

    Task1 way 3

--- HomeWork/Homework3/Program.cs
+++ HomeWork/Homework3/Program.cs
@@ -34,6 +34,35 @@ Console.ReadLine();
 
 // Решение 2:
 
+Console.Clear();
+int ReverseNumber(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result *= 10;
+        result = result + num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int reverseNum = ReverseNumber(num);
+
+if(num == reverseNum)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Число {num} - палиндром.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Число {num} НЕ является палиндромом.");
+}
+Console.ReadLine();
+
 /*
 Console.Clear();
 void Palindrome(int num)
@@ -52,6 +81,10 @@ Console.Write("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Palindrome(num);
 Console.ReadLine();
+*/
+// Решение 3:
+
+
 
 /*
 // Задача №2. Напишите программу, которая принимает на вход координаты двух точек и находит 
