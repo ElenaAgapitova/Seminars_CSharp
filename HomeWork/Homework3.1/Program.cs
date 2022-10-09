11d490bc9415b8eb02283f0aecb656d838e7ada1
Elena <agapitovae87@gmail.com>
2022-10-09 12:51:49 +0300

    Task1 way 3

---
+++ HomeWork/Homework3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача №1. 
+
+// Решение 3.
+
+Console.Clear();
+int ReverseNumber(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result *= 10;
+        result = result + num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int reverseNum = ReverseNumber(num);
+
+if(num == reverseNum)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Число {num} - палиндром.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Число {num} НЕ является палиндромом.");
+}
+Console.ReadLine();
