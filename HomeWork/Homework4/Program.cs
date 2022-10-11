1456df96ce27117dd6e9fac7aec9be2e8dbce0f2
Elena <agapitovae87@gmail.com>
2022-10-11 09:18:14 +0300

    Homework 4 Task 1 way 2 with for

--- HomeWork/Homework4/Program.cs
+++ HomeWork/Homework4/Program.cs
@@ -1,6 +1,26 @@
 ﻿// Задача №1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
 // в натуральную степень B.
+// Решение 1:
+Console.Clear();
+
+int DegreeAB(int a, int b)
+{
+    int result = a;
+    int size = b-1;
+    for(int count = 1; count <= size; count++)
+        result *= a;
+    return result;
+}
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int degreeAB = DegreeAB(a,b);
+Console.WriteLine($"Число {a} в степени {b} = {degreeAB}");
+Console.ReadLine();
+
 /*
+// Решение 2:
 Console.Clear();
 int DegreeAB(int a, int b)
 {
@@ -18,6 +38,7 @@ int degreeAB = DegreeAB(a,b);
 Console.WriteLine($"Число {a} в степени {b} = {degreeAB}");
 Console.ReadLine();
 */
+
 // Задача №2.  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 /*
 Console.Clear();
@@ -38,7 +59,7 @@ Console.WriteLine($"Сумма цифр в числе {num} = {sumDigits}");
 Console.ReadLine();
 */
 // Задача №3. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
+/*
 Console.Clear();
 
 void FillArray(int[] array)
@@ -63,5 +84,5 @@ FillArray(array);
 Console.Write("Массив из 8 элементов:");
 PrintArray(array);
 Console.ReadLine();
-
+*/
 
