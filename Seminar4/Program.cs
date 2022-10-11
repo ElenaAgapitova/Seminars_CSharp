1a7c21bd9ad7674afd32e1753bcf8614bc16d4ca
Elena <agapitovae87@gmail.com>
2022-10-11 10:54:33 +0300

    Seminar 4 Task 2,3

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
-
+/*
 Console.Clear();
 int GetSum(int a)
 {
@@ -12,4 +12,51 @@ Console.Write("Введите положительное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int result = GetSum(a);
 Console.WriteLine(result);
+*/
+// Задача №2:Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*
+Console.Clear();
+int NumberDigits(int num)
+{
+    int size = num.ToString().Length;
+    return size;
+}
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = NumberDigits(a);
+Console.WriteLine(result);
+Console.ReadLine();
+*/
+Console.Clear();
+int NumberDigits(int num)
+{
+    int count = 0;
+    while(num>0)
+    {
+        num/=10;
+        count++;
+    }
+    return count;
+}
+Console.Write("Введите положительное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = NumberDigits(a);
+Console.WriteLine(result);
+Console.ReadLine();
 
+// Задача №3:Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+/*
+Console.Clear();
+int Factorial(int a)
+{
+    int result = 1;
+    for (int current = 1; current <= a; current++)
+        result*= current;
+    return result;
+}
+Console.Write("Введите положительное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = Factorial(a);
+Console.WriteLine(result);
+Console.ReadLine();
+*/
